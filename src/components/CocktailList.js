698b8e2376493ba26be201a0f4ec61665bd0c0db
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-01-16 03:20:21 -0800

    cocktails fetch drinks

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
+import Loading from './Loading'
 
 const CocktailList = () => {
+  const {loading, cocktails} = useGlobalContext()
+  console.log(cocktails)
+  if(loading) {
+    return <loading />
+  }
+  if(cocktails.length < 1){
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
+  }
   return (
     <div>CocktailList</div>
   )
