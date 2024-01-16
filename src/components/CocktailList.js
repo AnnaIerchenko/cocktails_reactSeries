919f08da7da57044095e712f482781a59cfea2bc
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-01-16 05:29:23 -0800

    cocktails display list

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
 import Loading from './Loading'
+import Cocktail from './Cocktail'
 
 const CocktailList = () => {
   const {loading, cocktails} = useGlobalContext()
-  console.log(cocktails)
+
   if(loading) {
-    return <loading />
+    return <Loading />
   }
   if(cocktails.length < 1){
     return (
@@ -16,7 +17,14 @@ const CocktailList = () => {
     )
   }
   return (
-    <div>CocktailList</div>
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
   )
 }
 
