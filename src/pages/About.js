428deba06b3b32a396a07c19b28e9e70624bb27a
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-01-16 00:32:20 -0800

    init project, navbar, links to about, error and home  pages

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident ab asperiores distinctio veniam sit eum possimus doloribus! Odio harum nihil esse commodi, asperiores neque blanditiis. Aut ut magni architecto quis.
+        Repudiandae quidem exercitationem odio pariatur eos mollitia libero soluta facere doloribus, dignissimos veritatis, distinctio beatae ut voluptates. Modi saepe accusantium pariatur suscipit aliquam fuga. Iusto voluptates reiciendis modi animi maiores.
+      </p>
+    </section>
+  )
+}
+
+export default About
